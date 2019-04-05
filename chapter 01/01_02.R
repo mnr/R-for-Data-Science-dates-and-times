cd2b3f1407db02edfd0d7033ed8f9487dd9003fd
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-04-05 05:18:03 -0700

    posixct, posixlt

--- chapter 01/01_02.R
+++ chapter 01/01_02.R
@@ -10,15 +10,18 @@ as.integer(as.Date("1970-01-01")) # equals zero
 as.Date(as.Date("1970-01-01")+8046) # produces 1992-01-12 -- our original date
 
 # vectors can contain multiple dates
-HAL_online <- as.Date(c("1992-01-12", "1992-02-01"))
+HAL_online <- as.Date(c("1992-01-12", "1997-01-12"))
 str(HAL_online)
 HAL_online[2]
 
 # what about POSIX?
 # POSIXct stores date time as integer == # seconds since 1970-01-01 UTC
-HAL_online <- as.Date("1992-01-12")
-
+HAL_online <- as.POSIXct("1992-01-12 11:23:34", tz = "America/Chicago")
+as.integer(HAL_online)
 
 
 # POSIXlt stores date time as list:sec, min, hour, mday, mon, year, wday, yday, isdst, zone, gmtoff
-
+HAL_online <- as.POSIXlt("1992-01-12 11:23:34", tz = "America/Chicago")
+as.integer(HAL_online) # no longer an integer
+unclass(HAL_online) # this shows the components of the list
+month.name[HAL_online$mon +1] # equals January

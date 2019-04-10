961241b32a3c56ca16540f9d9589f05f82103a63
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-04-10 12:09:59 -0700

    initial commit

---
+++ chapter 02/02_08 formatting dates.R
@@ -0,0 +1,19 @@
+# formatting dates for output
+
+TimeNow <- Sys.time()
+
+# all the same
+strftime(TimeNow, "Time Now! Hour:%H, Minute:%m")
+format(TimeNow, "Time Now! Hour:%H, Minute:%m")
+as.character(TimeNow, "Time Now! Hour:%H, Minute:%m")
+
+
+# all of the modifiers
+for (converter in sort(c(letters,LETTERS))) {
+  modifier <- paste0("%",converter)
+  print(paste(modifier, 
+              TimeNow,
+              strftime(TimeNow, modifier),
+              sep = " ... "))
+}
+

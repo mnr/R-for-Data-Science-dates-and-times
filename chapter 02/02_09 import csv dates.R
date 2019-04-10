f298684092c8b23bbfcbf4dcacf4c1ead182cd58
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-04-10 16:23:39 -0700

    Initial commit

---
+++ chapter 02/02_09 import csv dates.R
@@ -0,0 +1,24 @@
+# import csv files with dates intact. 
+# Learn how to use colClasses = Date for POSIXct and POSIXlt.
+
+year_month_data <- read.csv("chapter 02/year_month_day_data.csv", 
+                            colClasses = c("Date","integer"),
+                            sep = ",",
+                            stringsAsFactors=FALSE)
+
+year_month_data <- read.csv("chapter 02/year_month_day_data.csv", 
+                              colClasses = c("POSIXct","integer"),
+                              stringsAsFactors=FALSE)
+
+
+# or...use the readr package
+# install.packages("readr")
+library(readr)
+
+rr_year_month_day_data <- read_csv("chapter 02/year_month_day_data.csv", 
+                                             col_types = cols(YearMonth = col_datetime(format = "%Y-%m-%d %H:%M:%S")))
+                                             View(year_month_day_data)
+
+year_month_data <- read_csv("chapter 01/year_month_data.csv", 
+                            col_types = cols(YearMonth = col_date(format = "%Y-%m")))
+                                             

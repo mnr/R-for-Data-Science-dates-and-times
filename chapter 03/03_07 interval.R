cf4bf1eb08b18ede42f27f3b780052d42a5a6647
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-04-16 13:41:22 -0700

    additional interval operators

--- chapter 03/03_07 interval.R
+++ chapter 03/03_07 interval.R
@@ -17,3 +17,10 @@ LD_JRTdiff/years(1) # periods understand leap years
 LD_JRTdiff/dyears(1) # durations don't
 round(LD_JRTdiff/dyears(1))
 
+# other lubridate interval operators
+JRT_birth_death <- interval(JRTolkienBday,mdy("September 2, 1973"))
+mdy("Jul 28, 1914") %within% JRT_birth_death # did WWI start during Tolkien's lifetime?
+int_start(JRT_birth_death) # when was he born?
+int_length(JRT_birth_death) # interval in seconds
+int_length(int_shift(JRT_birth_death, years(1))) # length of interval if JRT was born 1 year later
+

04034e13978e3298871b890ee210903753b0c590
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-04-24 12:55:31 -0700

    small corrections

--- chapter 05/05_06 rolling averages.R
+++ chapter 05/05_06 rolling averages.R
@@ -5,6 +5,7 @@
 install.packages("zoo")
 library(zoo)
 
+# Grab some data ----------------------------------------------------------
 weatherData <- read.table("https://raw.githubusercontent.com/lyndadotcom/LPO_weatherdata/master/Environmental_Data_Deep_Moor_2012.txt",
                           header = TRUE,
                           stringsAsFactors = FALSE)
@@ -16,9 +17,22 @@ zooweather_order <- as.POSIXct(paste(weatherData[ , 1],weatherData[,2]),
 
 newZoo <- zoo(x = zooweather_data, order.by = zooweather_order)
 
+
+# Time Series and Statistics ----------------------------------------------
 rollMean_air_temp <- rollmean(newZoo$Air_Temp, k = 100)
-head(rollMean_air_temp)
-rollMax_air_temp_10 <- rollmax(newZoo$Air_Temp, k = 51)
+rollMax_air_temp <- rollmax(newZoo$Air_Temp, k = 51)
 
 plot(newZoo$Air_Temp, col = "yellow", type = "p")
 lines(rollMean_air_temp, col = "red")
+
+# rollapply ---------------------------------------------------------------
+myRollingFunction <- function(x) {
+  # assume that x is a vector with length of "width"
+  paste("Delta =", round(max(x) - min(x), digits = 3) ) # do something here
+}
+
+myRollingFunction(c(3,5,7.334567))
+
+rollapply(data = newZoo$Air_Temp, 
+          width = 10,
+          FUN = myRollingFunction)

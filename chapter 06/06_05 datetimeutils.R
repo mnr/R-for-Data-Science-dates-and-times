232152dd7f387920eb0877b3429de966d2802b5f
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-04-30 14:36:38 -0700

    adding notes

--- chapter 06/06_05 datetimeutils.R
+++ chapter 06/06_05 datetimeutils.R
@@ -5,12 +5,19 @@ library(datetimeutils)
 
 MaryShellyFrankenstein <- as.Date( "1 January 1818", "%d %B %Y")
 
+# these are only a sample of the available utilities
+?is_weekend
 is_weekend(MaryShellyFrankenstein)
 next_businessday(MaryShellyFrankenstein)
 
 convert_tz(MaryShellyFrankenstein, from = "PST", to = "EST")
 
+?first_of_month
 first_of_month(MaryShellyFrankenstein)
 end_of_month(MaryShellyFrankenstein)
 
 month(MaryShellyFrankenstein)
+
+timegrid(from = as.POSIXct("2100-03-23 12:34:01"), 
+         to = as.POSIXct("2100-04-01 12:33:00"),
+         interval = "1 hour")
